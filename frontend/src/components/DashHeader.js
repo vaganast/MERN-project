19525bdb2ba1c622d62f8a212388b022de2692cf
vaganast <71605687+vaganast@users.noreply.github.com>
2022-10-11 20:09:15 +0300

    fix bug route

--- frontend/src/components/DashHeader.js
+++ frontend/src/components/DashHeader.js
@@ -6,7 +6,7 @@ const DashHeader = () => {
     const content = (
         <header className="dash-header">
             <div className="dash-header__container">
-                <Link to="/dash/notes">
+                <Link to="/dash">
                     <h1 className="dash-header__title">techNotes</h1>
                 </Link>
                 <nav className="dash-header__nav">
