8cf84b316542a190ce2bea2d5943e54910cd75a1
vaganast <71605687+vaganast@users.noreply.github.com>
2022-10-12 16:04:37 +0300

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000/', //edw tha einai i react
+    'http://localhost:3000', //edw tha einai i react
     'https://www.dandrepairshop.com', //locals host gia accept to api mas
     'https://dandrepairshop.com'
 ]
