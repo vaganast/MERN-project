f0da2c863041c7e2cf835be7c712b43d59e8b6c8
vaganast <71605687+vaganast@users.noreply.github.com>
2022-10-15 14:57:39 +0300

    verifyJWT midd
    apply the middleware  to user and notes routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/usersController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+router.use(verifyJWT) //apply to all routes inside here
 
 router.route('/')
     .get(usersController.getAllUsers)
