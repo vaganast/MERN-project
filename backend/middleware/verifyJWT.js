276f69af9b5201e90d51415043758e1eaeab8bfc
vaganast <71605687+vaganast@users.noreply.github.com>
2022-10-15 14:54:39 +0300

    Update verifyJWT.js

--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -14,9 +14,9 @@ const verifyJWT = (req, res, next) => {
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
             if (err) return res.status(403).json({ message: 'Forbidden' })
-            req.user = decoded.UserInfo.username;
-            req.roles = decoded.UserInfo.roles;
-            next();
+            req.user = decoded.UserInfo.username
+            req.roles = decoded.UserInfo.roles
+            next()
         }
     )
 }
