56345e25cb4895905882ea3fe3caa42ac4faa4c3
vaganast <71605687+vaganast@users.noreply.github.com>
2022-10-20 18:57:11 +0300

    Update Welcome.js
    only manager or admin can see user settings or add a new user

--- frontend/src/features/auth/Welcome.js
+++ frontend/src/features/auth/Welcome.js
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom'
 import useAuth from '../../hooks/useAuth'
 
 const Welcome = () => {
-    const { username, status } = useAuth()
+    const { username, isManager, isAdmin } = useAuth()
+
     const date = new Date()
     const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
 
@@ -11,15 +12,15 @@ const Welcome = () => {
 
             <p>{today}</p>
 
-            <h1>Welcome!</h1>
+            <h1>Welcome! {username}</h1>
 
             <p><Link to="/dash/notes">View techNotes</Link></p>
 
             <p><Link to="/dash/notes/new">Add New techNotes</Link></p>
 
-            <p><Link to="/dash/users">View User Settings</Link></p>
+            {(isManager || isAdmin) && <p><Link to="/dash/users">View User Settings</Link></p>}
 
-            <p><Link to="/dash/users/new">Add New User</Link></p>
+            {(isManager || isAdmin) && <p><Link to="/dash/users/new">Add New User</Link></p>}
 
         </section>
     )
