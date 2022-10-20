90eb5f38e48a8c9ebe39a5ec14f694f465882083
vaganast <71605687+vaganast@users.noreply.github.com>
2022-10-20 20:00:37 +0300

    Update App.js
    Protect the routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,24 +12,30 @@ import EditNote from './features/notes/EditNote'
 import NewNote from './features/notes/NewNote'
 import Prefetch from './features/auth/Prefetch'
 import PersistLogin from './features/auth/PersistLogin'
+import RequireAuth from './features/auth/RequireAuth'
+import { ROLES } from './config/roles'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        {/* Public Routes */}
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
+        {/* Protected Routes */}
         <Route element={<PersistLogin />}>
+        <Route element={<RequireAuth allowedRoles={[...Object.values(ROLES)]}/>}>
         <Route element={<Prefetch />}>
         <Route path="dash" element={<DashLayout />}>
           
           <Route index element={<Welcome />} />
-
+          <Route element={<RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]}/>}>
           <Route path="users">
             <Route index element={<UserList />} />
             <Route path=":id" element={<EditUser />} />
             <Route path="new" element={<NewUserForm />} />
+          </Route>
           </Route>
 
           <Route path="notes">
@@ -38,9 +44,10 @@ function App() {
             <Route path="new" element={<NewNote />} />
           </Route>
 
-        </Route> {/* End DashLayout */}
-      </Route>
-    </Route>
+          </Route> {/* End DashLayout */}
+        </Route>
+        </Route>
+      </Route>{/* End Protected Routes */}
         
       </Route>
     </Routes>
