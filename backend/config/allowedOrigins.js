47bd16ddf25b6069f26444b6f6a08c3224e628f6
vaganast <71605687+vaganast@users.noreply.github.com>
2022-10-23 19:04:00 +0300

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,10 +1,10 @@
 const allowedOrigins = [
    'https://mern-project-vaganast.vercel.app',
-   'https://mern-project-l6p12ad4c-vaganast.vercel.app/',
-   'https://mern-project-105rn74iz-vaganast.vercel.app/',
-   'https://mern-project-omega.vercel.app/',
-   'https://mern-project-git-main-vaganast.vercel.app/',
-   'https://mern-project-vaganast.vercel.app/',
+   'https://mern-project-l6p12ad4c-vaganast.vercel.app',
+   'https://mern-project-105rn74iz-vaganast.vercel.app',
+   'https://mern-project-omega.vercel.app',
+   'https://mern-project-git-main-vaganast.vercel.app',
+   'https://mern-project-vaganast.vercel.app',
    'https://mern-frontend.onrender.com'
 ]
 
