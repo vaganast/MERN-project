c7a59ea82a2e72bca7b9fed35895fda5c8895bc7
vaganast <71605687+vaganast@users.noreply.github.com>
2022-10-23 16:32:56 +0300

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
    'https://mern-project-vaganast.vercel.app/',
-   'https://mern-project-l6p12ad4c-vaganast.vercel.app/'
+   'https://mern-project-l6p12ad4c-vaganast.vercel.app/',
+   'https://mern-project-105rn74iz-vaganast.vercel.app/'
 ]
 
 module.exports = allowedOrigins
