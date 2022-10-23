5fd34132b76376f336e5fdf9a997d4bf6c823d0c
vaganast <71605687+vaganast@users.noreply.github.com>
2022-10-23 16:12:51 +0300

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000', //edw tha einai i react
-    'https://www.dandrepairshop.com', //locals host gia accept to api mas
-    'https://dandrepairshop.com'
+   'https://mern-project-vaganast.vercel.app/'
 ]
 
 module.exports = allowedOrigins
