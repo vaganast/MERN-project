d327f6bac7cf3d3aa8b73b4240d415cb9a525362
vaganast <71605687+vaganast@users.noreply.github.com>
2022-10-23 15:56:22 +0300

    bug
    fix a typo error in class of inactive user
    fix some bugs

--- frontend/src/features/users/User.js
+++ frontend/src/features/users/User.js
@@ -20,7 +20,7 @@ const User = ({ userId }) => {
 
         const userRolesString = user.roles.toString().replaceAll(',', ' ,')
         
-        const cellStatus = user.active ? '' : 'table__cell- -inactive'
+        const cellStatus = user.active ? '' : 'table__cell--inactive'
 
         return (
             <tr className="table__row user">
