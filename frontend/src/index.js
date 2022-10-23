ab47d43d47f1c9151856f0e60df5502cca55144e
vaganast <71605687+vaganast@users.noreply.github.com>
2022-10-23 15:02:28 +0300

    changes for deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+import { processResult } from 'immer/dist/internal';
+
+if (processResult.env.Node_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
